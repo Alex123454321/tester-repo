c41f944ed30dafe8cfd78ab1cbbc0d7cc4c7b725
Alex123454321 <alex_moiseev_s@mail.ru>
2016-03-25 15:26:57 +0300

    Update ЗаготовкаСмоука2.java

--- ЗаготовкаСмоука2.java
+++ ЗаготовкаСмоука2.java
@@ -1,64 +1,42 @@
-package com.example.tests;
-
 import com.thoughtworks.selenium.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.io.FileWriter;
 import java.util.regex.Pattern;
 
-public class ЗаготовкаСмоука2 {
+
+public class TIN_15 {
 	private Selenium selenium;
 
 	@Before
 	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://tinko.rtz.io/");
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://vs7.io/");
 		selenium.start();
 	}
 
 	@Test
-	public void testЗаготовкаСмоука2() throws Exception {
-		selenium.open("http://tinko.rtz.io/p-247997.html");
+	public void test123() throws Exception {
+     	selenium.open("http://tinko.rtz.io/");
 		selenium.waitForPageToLoad("20000");
-		selenium.click("xpath=(//button[@type='button'])[6]");
+		selenium.click("link=Личный кабинет");
 		for (int second = 0;; second++) {
 			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("link=Корзина - 1 поз.")) break; } catch (Exception e) {}
+			try { if (selenium.isElementPresent("id=mini-login")) break; } catch (Exception e) {}
 			Thread.sleep(1000);
 		}
-
-		selenium.click("link=Корзина - 1 поз.");
-		selenium.click("xpath=(//button[@type='button'])[4]");
+		selenium.type("id=mini-login", "Strelok201995@mail.ru");
+		selenium.type("id=mini-password", "4793546224");
+		selenium.click("css=button.btn.btn-primary");
 		selenium.waitForPageToLoad("20000");
-		selenium.click("id=billing:legal_entity");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("id=billing:company")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
-		selenium.click("id=shipping:different_shipping");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("id=shipping:firstname")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
-		selenium.click("id=shipping:legal_entity");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("id=shipping:company")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
-		selenium.click("id=s_method_storepickup_storepickup");
-		selenium.click("id=s_method_freeshipping_freeshipping");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isElementPresent("id=shipping:street1")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
+		assertEquals("Вход с учетной записью", selenium.getText("css=div.auth > h2"));
+		try(FileWriter writer = new FileWriter("C:\\Users\\Алексей\\Desktop\\Logs.txt",  true))
+        {        
+            writer.write("TIN-15: Тест успешно пройден. Перенаправление на нужную страницу работает правильно. "+"\r\n");  
+            writer.flush();
+        }
 	}
 
 	@After
